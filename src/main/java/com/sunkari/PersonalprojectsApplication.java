4b048b1f4419a06e144ae0ee15c82441722fc810
Prashant Sunkari <sunkari@olympus.f5net.com>
2017-01-29 23:33:42 -0800

    Initial commit

---
+++ src/main/java/com/sunkari/PersonalprojectsApplication.java
@@ -0,0 +1,12 @@
+package com.sunkari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalprojectsApplication.class, args);
+	}
+}
